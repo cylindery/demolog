f2faa84d4ec1264ee24eecf9ae2762fec6ff9888
juwonju <jxwxnjx@gmail.com>
2023-05-15 17:12:12 +0900

    데이터 검증-1

--- src/main/java/com/demolog/api/request/PostCreate.java
+++ src/main/java/com/demolog/api/request/PostCreate.java
@@ -1,23 +1,20 @@
 package com.demolog.api.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@ToString
 public class PostCreate {
 
+    @NotBlank(message = "타이틀을 입력해주세요.")
     public String title;
+
+    @NotBlank(message = "콘텐츠를 입력해주세요.")
     public String content;
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "PostCreate{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
