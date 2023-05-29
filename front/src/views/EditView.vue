53d5c78598f4a74705e3f5f71a9a28a74914d4dc
juwonju <jxwxnjx@gmail.com>
2023-05-29 20:46:24 +0900

    화면 예쁘게 만들기

--- front/src/views/EditView.vue
+++ front/src/views/EditView.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
+import { ref, defineProps } from "vue";
 
-import {ref} from "vue";
-import axios from 'axios';
-import {useRouter} from "vue-router";
-import {defineProps} from "vue/dist/vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
 
 const post = ref({
   id: 0,
   title: "",
   content: "",
 });
-
-const router = useRouter();
 
 const props = defineProps({
   postId: {
@@ -26,25 +25,23 @@ axios.get(`/api/posts/${props.postId}`).then((response) => {
 
 const edit = () => {
   axios.patch(`/api/posts/${props.postId}`, post.value).then(() => {
-    router.replace({name: "home"})
+    router.replace({ name: "home" });
   });
-}
+};
 </script>
 
 <template>
   <div>
-    <el-input v-model="post.title"/>
+    <el-input v-model="post.title" />
   </div>
 
   <div class="mt-2">
-    <el-input v-model="post.content" type="textarea" rows="15"/>
+    <el-input v-model="post.content" type="textarea" rows="15" />
   </div>
 
-  <div class="mt-2">
+  <div class="mt-2 d-flex justify-content-end">
     <el-button type="warning" @click="edit()">수정완료</el-button>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import { ref } from "vue";
 
-import axios from 'axios';
-import {useRouter} from "vue-router";
+import axios from "axios";
+import { useRouter } from "vue-router";
 
 const title = ref("");
 const content = ref("");
@@ -11,30 +11,30 @@ const router = useRouter();
 
 const write = function () {
   axios
-      .post("/api/posts", {
-        title: title.value,
-        content: content.value
-      })
-      .then(() => {
-        router.replace({name: "home"})
-      });
+    .post("/api/posts", {
+      title: title.value,
+      content: content.value,
+    })
+    .then(() => {
+      router.replace({ name: "home" });
+    });
 };
 </script>
 
 <template>
   <div>
-    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+    <el-input v-model="title" placeholder="제목을 입력해주세요" />
   </div>
 
   <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15"/>
+    <el-input v-model="content" type="textarea" rows="15" />
   </div>
 
   <div class="mt-2">
-    <el-button type="primary" @click="write()">글 작성완료</el-button>
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
+    </div>
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>
